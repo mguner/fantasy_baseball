7dfd955b7d9fe8d2c9ef054a44a842ed04242b0f
Murat Güner <mrtgnr@murats-air.myfiosgateway.com>
2022-02-02 21:16:44 -0500

    codes has been organized

--- main_app.py
+++ main_app.py
@@ -7,31 +7,53 @@ import pandas as pd
 import datetime
 import plotly.graph_objects as go
 import numpy as np
+import os
 from utils import *
 
 # title of the site
 st.title('CBS Fantasy Baseball Data Analysis')
 
 # Example url for the user
-st.write('Example URL: https://www.cbssports.com/fantasy/baseball/players/2507363/fernando-tatis/')
+st.write("Find a Player from CBS Website: https://www.cbssports.com/fantasy/baseball/stats/") 
 
+
+with st.expander("See more explanation"):
+     st.write("""
+         When you go to the link given above, find the player
+         whose data you would like to analyse. Click on the name of the player and 
+         this will take you to the player's page. You need to copy it and paste it below.
+     """)
+     st.image("images/cbs_stats.png", use_column_width= True)
+     st.write("""As you see below, if you click on the name of the Fernando Tatis
+     you will end up in the player page. Copy the link and paste is below.
+    """)
+     st.image("images/tatis.png", use_column_width= True)
 # take the user input
 url = st.text_input('Enter the url')
+url = url.strip()
 
+if not url:
+    url = 'https://www.cbssports.com/fantasy/baseball/players/2507363/fernando-tatis/'
 # show user input url
 st.write('you entered:', url)
 
+with st.expander("Expand if it takes too long"):
+     st.write("""
+         If the player you choose has 10+ years of history in MLB 
+         taking all of their data might take some time.
+         Even in the worst case scenario this shouldn't take more than a couple of minutes though.
+     """)
 # get data and years for the player
 dataframe, years = stats(url = url)
 
 # user select a metric
 selected_metric = st.selectbox(
-     'What are your stats you would like to focus',
+     'What are your stats you would like to focus. You can select one and change it later.',
      ('AVG', 'SLG', 'OBP'))
 
 # user select years
 selected_years = st.multiselect(
-     'What are the years you would like to focus',
+     'What are the years you would like to focus. You can select multiple and change later again.',
      years,
      ['2019'])
 
@@ -45,4 +67,7 @@ for year in selected_years:
                     mode='lines',
                     name= year + " " + selected_metric))
 
-st.plotly_chart(fig)
+with st.container():
+    player_name = dataframe.player_name.unique()[0]
+    st.write(f'Player: {player_name}')
+    st.plotly_chart(fig)
--- utils.py
+++ utils.py
@@ -1,13 +1,23 @@
 import streamlit as st
 import requests
 from bs4 import BeautifulSoup as bs
-import re
 import csv
 import pandas as pd
-import datetime
+from datetime import datetime
 import os
 
+# gather urls for each year
 def yearLinks(soup):
+    """
+    Each year data for a player shown in a different page. 
+    This function creates a list of urls from a given soup object of the player's page.
+    Parameters
+    --------
+    soup: bs4.BeautifulSoup object. Created from player's page html.
+    Returns
+    ---------
+    links: list. List of strings. Each string links to the player's page for different year's data.
+    """
     links = []
     base_url = 'https://www.cbssports.com'
     drop_down = soup.find_all('ul', 'Dropdown-list')[0]
@@ -15,44 +25,80 @@ def yearLinks(soup):
         links.append(base_url + url['href'] )
     return links
 
-@st.cache
-def stats(url):
-    years = []
-    header_dictionary = {'AVG':-4, 'SLG':-3, 'OBP':-2 } 
-    field_names = ['player_name', 'date'] + ['AVG', 'SLG', 'OBP']
-    metrics = ['AVG', 'SLG', 'OBP']
+# get name and id from url
+def player_info_from_url(url):
     player_name = url.split('/')[-2]
     player_id = url.split('/')[-3]
-    player_url = f'https://www.cbssports.com/fantasy/baseball/players/game-log/2021/{player_id}/{player_name}/'
+    return player_name, player_id
+
+# given year, player_name and id create the url.
+def create_url(year, player_name, player_id):
+    base = 'https://www.cbssports.com/'
+    extension = f'fantasy/baseball/players/game-log/{year}/{player_id}/{player_name}/'
+    return base+extension
+
+# from a url create soup object
+def make_soup(url):
+    response = requests.get(url)
+    soup = bs(response.content, features="html.parser")
+    return soup
+
+# cache so that page would not refresh each time.
+@st.cache
+def stats(url):
+    # to keep the list of years a player played in MLB
+    years = []
+    # in the tables(html) the indexes of metrics
+    header_dictionary = {'AVG':-4, 'SLG':-3, 'OBP':-2 } 
+    field_names = ['player_name', 'date'] + ['AVG', 'SLG', 'OBP']
+    # we can add additional metrics later on
+    metrics = ['AVG', 'SLG', 'OBP']
+    # get player name and id
+    player_name, player_id = player_info_from_url(url)
+    # create an url for one of the player's pages.
+    player_url = create_url('2021', player_name, player_id)
+    # create a temporary csv file. Not sure this is needed.
     with open(f'{player_name}.csv', 'w', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=field_names)
         writer.writeheader()
-        response = requests.get(player_url)
-        soup = bs(response.content, features="html.parser")
+        # create soup
+        soup = make_soup(player_url)
+        # create links for each year
         links = yearLinks(soup)
         for url in links:
+            # get the year
             year = url.split('/')[-4]
-            response = requests.get(url)
-            soup = bs(response.content, features="html.parser")
+            # soup for a particular year data
+            soup = make_soup(url)
             try:
-                table = soup.find_all('table', 'data compact')[0]
+                # main table in the page
+                table = soup.find('table', 'data compact')
+                # append year in try so that if table is empty user don't see it
                 years.append(year)
+                # data kept in monthly - each month data is a table.
                 for month in table.findAll('tbody'):
+                    # we run through each row 
                     for row in month.findAll('tr'):
                         row_data = {}
+                        # get player data - for consistency
                         row_data['player_name'] = player_name 
+                        # day of the game that data is coming from
                         day = row.findAll('td')[0].text
-                        date = datetime.datetime.strptime(day + '/'+ year, '%m/%d/%Y' )
+                        # add the year to date format
+                        date = datetime.strptime(day+ '/' + year, '%m/%d/%Y' )
                         row_data['date'] = date
+                        # focus only on the metrics listed at the begining
                         for s in metrics:
                             s_index = header_dictionary[s]
                             row_data[s] = float(row.findAll('td')[s_index].text)
                         writer.writerow(row_data)
             except:
                 print('No content for:', url)
+    # read the csv to a dataframe
     df = pd.read_csv(f'{player_name}.csv',
                          index_col = 'date', 
                          parse_dates= True, 
                          infer_datetime_format= True)
+    # delete the csv 
     os.remove(f'{player_name}.csv')
     return df, years
